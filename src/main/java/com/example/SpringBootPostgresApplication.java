27a2b4c9bfe2dd6d34a4d2aaf91227fd0b7a9174
Art Sak <art@odd.works>
2017-05-26 17:31:13 +0700

    Add CLR example.

--- src/main/java/com/example/SpringBootPostgresApplication.java
+++ src/main/java/com/example/SpringBootPostgresApplication.java
@@ -1,14 +1,27 @@
 package com.example;
 
+import com.example.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class SpringBootPostgresApplication {
+public class SpringBootPostgresApplication implements CommandLineRunner {
+
+    @Autowired
+    private ProductService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPostgresApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (service != null)
+            System.out.println("Good to Go " + service.toString());
+    }
+
 }
